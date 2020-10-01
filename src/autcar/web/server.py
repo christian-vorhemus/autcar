9860654bae082302d5862f3c960a9ce261e4385f
Christian Vorhemus <chris.vorhemus@gmail.com>
2020-10-01 18:12:39 +0200

    Update server.py

--- src/autcar/web/server.py
+++ src/autcar/web/server.py
@@ -130,4 +130,4 @@ def video():
 
  
 if __name__ == "__main__":
-	app.run(debug = False, host='0.0.0.0', port=8080, passthrough_errors=True)
+    app.run(debug = False, host='0.0.0.0', port=8080, passthrough_errors=True)
